d9157fb783483ba767cff4197f3b5533ee4f031e
Abhishekkatale <abhishekkatale427@gmail.com>
2025-06-11 11:17:55 +0530

    new data add

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
+import { BrowserRouter } from "react-router-dom"; // ✅ import this
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+  <BrowserRouter> {/* ✅ wrap App in BrowserRouter */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
